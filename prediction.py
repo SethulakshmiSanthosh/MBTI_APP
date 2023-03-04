1f6e301243b129a6e42723cc9eaf27ac054a0717
Sethulakshmi Santhosh <sethulakshmisanthosh2002@gmail.com>
2023-03-04 16:10:23 +0530

    one1

---
+++ prediction.py
@@ -0,0 +1,76 @@
+import joblib
+from preprocessing import prep_data
+import time
+import sklearn
+
+###############################################################################
+#            FUNCTION TO COMBINE THE RESULTS OF THE 4 MODELS                  #
+###############################################################################
+
+def predict(s):
+    return len(s.split(" "))
+
+def trace_back(combined):
+    type_list = [
+        {"0": "I", "1": "E"},
+        {"0": "N", "1": "S"},
+        {"0": "F", "1": "T"},
+        {"0": "P", "1": "J"},
+    ]
+    result = []
+    for num in combined:
+        s = ""
+        for i in range(len(num)):
+            s += type_list[i][num[i]]
+        result.append(s)
+    return result
+
+def combine_classes(y_pred1, y_pred2, y_pred3, y_pred4):
+    combined = []
+    for i in range(len(y_pred1)):
+        combined.append(
+            str(y_pred1[i]) + str(y_pred2[i]) + str(y_pred3[i]) + str(y_pred4[i])
+        )
+    result = trace_back(combined)
+    return result[0]
+###############################################################################
+
+
+
+###############################################################################
+#                           MODEL PREDICTIONS                                 #
+###############################################################################
+
+def prediction(s):
+
+    X = prep_data(s)
+
+    # loading the 4 modelS
+    EorI_model = joblib.load("./models/clf_is_Extrovert.joblib")
+    SorN_model = joblib.load("./models/clf_is_Sensing.joblib")
+    TorF_model = joblib.load("./models/clf_is_Thinking.joblib")
+    JorP_model = joblib.load("./models/clf_is_Judging.joblib")
+
+    
+    # predicting
+    EorI_pred = EorI_model.predict(X)
+    SorN_pred = SorN_model.predict(X)
+    TorF_pred = TorF_model.predict(X)
+    JorP_pred = JorP_model.predict(X)
+    
+    # combining the predictions from the 4 models
+    result = combine_classes(EorI_pred, SorN_pred, TorF_pred, JorP_pred)
+
+    return result
+    
+
+###############################################################################
+#                                   MAIN                                      #
+###############################################################################
+
+if __name__ == "__main__":
+    t = time.time()
+    string = "I just wanna to go home!!!!!! :sadpanda: https://www.youtube.com/watch?v=TQP20LTI84A"
+    print(prediction(string))
+    print(f"Preprocessing Time: {time.time() - t} seconds")
+ 
---
+++ server.py
@@ -0,0 +1,46 @@
+from flask import *;
+from prediction import prediction;
+app = Flask(__name__)
+
+###############################################################################
+#                       SETTING UP APP ROUTES                                 #
+###############################################################################
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/response", methods=["GET", "POST"])
+def response():
+
+    if request.method == "POST":
+        snippet = request.form["fsnippet"]
+        # Testing with predict.py
+        print(snippet)
+        personality_type = prediction(snippet)
+    return render_template("response.html", name=personality_type, string=snippet)
+
+'''
+@app.route("/analysis")
+def analysis():
+    return render_template("analysis.html")
+
+
+@app.route("/methodology")
+def methodology():
+    return render_template("methodology.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+'''
+
+###############################################################################
+#                                   MAIN                                      #
+###############################################################################
+
+if __name__ == "__main__":
+    app.run(debug=True)
